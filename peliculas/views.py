847e294efe6358a0d21d5bfd224b09aebadf99ad
Sergio Pérez Aldavalde <143305775+SrArmstrong@users.noreply.github.com>
2025-02-13 12:00:39 -0600

    Nuevos Usuarios y Roles... Creo

--- peliculas/views.py
+++ peliculas/views.py
@@ -2,8 +2,19 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Pelicula
 from .serializers import PeliculaSerializer
+from .permissions import EsAdministrador, EsEliminador, EsVisualizador
 
 class PeliculaViewSet(viewsets.ModelViewSet):
     queryset = Pelicula.objects.all()
     serializer_class = PeliculaSerializer
     permission_classes = [IsAuthenticated] # Autentificación de usuario
+
+    def get_permissions(self):
+        "Asigna permisos según el método HTTP."
+        if self.request.method in ['POST', 'PUT']:
+            return [EsAdministrador()]
+        elif self.request.method == 'DELETE':
+            return [EsEliminador()]
+        elif self.request.method == 'GET':
+            return [EsVisualizador()]
+        return super().get_permissions()
